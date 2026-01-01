4e1a48dd60e323495f83bc733d686e7292815a89
joey <koentarij@gmail.com>
2026-01-01 17:19:42 +0100

    back1b

--- BA/base/baseEnvironment.py
+++ BA/base/baseEnvironment.py
@@ -65,5 +65,5 @@ class DroneEnvironment(DirectRLEnvCfg):
     )
 
     def __init__(self):
-        self.drone = Drone(num_envs)
+        self.drone = Drone(4)
         self.drone.articulation_cfg.replace(prim_path="/World/envs/env_.*/drone")

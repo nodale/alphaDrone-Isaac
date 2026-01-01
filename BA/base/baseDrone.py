3f865f369ca82c4d83c17e44e794cf39e915bb3c
joey <koentarij@gmail.com>
2026-01-01 16:40:17 +0100

    .

--- BA/base/baseDrone.py
+++ BA/base/baseDrone.py
@@ -11,6 +11,7 @@ class Drone():
 
     articulation_cfg : ArticulationCfg = ArticulationCfg(
                 spawn=sim_utils.UsdFileCfg(usd_path="drone/test9.usda"),
+                prim_path="/World/envs/env_.*/drone",
                 actuators={"rotors": ImplicitActuatorCfg(joint_names_expr=["rotor_[1-4]_joint"], damping=None, stiffness=None)},
                 init_state=ArticulationCfg.InitialStateCfg(pos=[0.0, 0.0, 0.2])
                 )   

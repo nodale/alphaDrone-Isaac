48cf976ecf7a6cfcb5a510ab156adbdc882f7501
joey <koentarij@gmail.com>
2026-01-01 17:54:03 +0100

    clean_1b

--- alphaDroneClean/task.py
+++ alphaDroneClean/task.py
@@ -2,10 +2,10 @@ import gymnasium as gym
 
 gym.register(
     id="run",
-    entry_point=f"alphaDroneEnv:QuadcopterEnv",
+    entry_point=f"BA:DroneEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"alphaDroneEnv:QuadcopterEnvCfg",
+        "env_cfg_entry_point": f"BA:DroneEnvCfg",
         "skrl_cfg_entry_point": f"agent:skrl_ppo_cfg.yaml",
     },
 )
